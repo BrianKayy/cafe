5796358ff04e747173aaa54627ae9a6cf6fba779
BrianKayy <Briankasumba40@gmail.com>
2024-01-29 16:24:08 +0400

    last edit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter  as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from "./Home";
-import About from "./About";
+import About from './About'
 
  import Footer from "./Footer";
 
